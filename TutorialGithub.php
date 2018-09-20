2f2f7aaa338b1a16fb427b4c52d46286c11c73d6
mrsundaymonday <mrsundaymonday@gmail.com>
2018-09-20 12:05:37 +0700

    Penambahan content tutorial Undo/Reverse

--- TutorialGithub.php
+++ TutorialGithub.php
@@ -23,8 +23,26 @@ Tutorial git hub
 			  3. git commit -m "pesan"
 			  4. git push origin master
 
-			C. Undo Push / Reverse Commit
-				Harus cari tutorial baru
+			C. log & Diff
+				Log untuk melihat log perubahan terakhir. sedangkan diff digunakan untuk membandingkan perubahan apa saja yang telah dilakukan terhadap file.
+
+			  1. Log
+			  	 command: # git log / # git log [nama file] /  # git log [kode commit]
+			  2. Diff
+			  	 command: #git diff [kode commit]
+
+			D. Undo Push / Reverse Commit
+			   Terdapat 3 Kondisi yaitu:
+				1. Kondisi Sebelum stage 
+					command: # git checkout [nama file]
+					command ini sangat berbahaya, dapat menghapus isi dari file yang telah dibuat.
+
+				2. Kondisi sudah di Stage
+					command: # git reset [nama file]
+					mengembalikan kondisi dari staged kedalam kondisi modified lagi.
+
+				3. Kondisi sudah commit
+					command: #
 
 
 
