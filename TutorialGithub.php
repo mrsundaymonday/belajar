5bdc05137718df1964a6cea587405c62b0e62773
mrsundaymonday <mrsundaymonday@gmail.com>
2018-09-20 11:03:23 +0700

    Upload file ke dua

---
+++ TutorialGithub.php
@@ -0,0 +1,33 @@
+Tutorial git hub
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tutorial Github</title>
+</head>
+<body>
+	<pre>
+		<code>
+			* login dan create repo di github
+
+			* copy link repo misal : "https://github.com/mrsundaymonday/belajar.git"
+
+			A. Clone(Download)
+				Masuk ke lokasi folder yang ingin di clone
+			  1. git clone https://github.com/mrsundaymonday/belajar.git
+
+			B. Push(Upload)
+			  1. git init
+			  2. git add .(semua) / git add [namafile](per nama file)
+			  3. git commit -m "pesan"
+			  4. git push origin master
+
+			C.			
+		</code>		
+	</pre>
+
+</body>
+</html>
+ 
+
