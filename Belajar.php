008433178bc51e5b68d16164fd54d540b01d032f
mrsundaymonday <mrsundaymonday@gmail.com>
2018-09-20 10:51:34 +0700

    Publish belajar PHP file

---
+++ Belajar.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Belajar github</title>
+</head>
+<body>
+	<center>
+	<h1>Belajar Github</h1>
+	<p>Ini merupakan file php untuk latihan belajar git</p>
+</center>
+</body>
+</html>
